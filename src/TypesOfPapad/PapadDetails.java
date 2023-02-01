1a17dd632d8e4f2e44d16a9fba9eab0b4c488edc
Nandu-Chopade <nanduchopade1998@gmail.com>
2023-02-01 23:02:19 +0530

    commit nothing new interface file deleted

--- src/TypesOfPapad/PapadDetails.java
+++
@@ -1,9 +0,0 @@
-package TypesOfPapad;
-
-public interface PapadDetails {
-	
-	public static int CalculationOfRevenue(int quantity , int totalBill) {
-		return totalBill = quantity * 110 ; 
-	}
-	
-}
